9b7f0717846bf8bcd80359b58a0dbeebca2efa60
Abdul Rahman <iamarsiddiqui@gmail.com>
2023-07-29 03:31:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+import express from 'express';
+import ejs from 'ejs';
+import path from 'path';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { config } from 'dotenv';
+config()
+
+// mongoose connection settings
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: "backend",
+}).then(() => console.log("mongodb connected")).catch((e) => console.log(e));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+const app = express();
+
+// middleware setup
+app.use(express.static(path.join(path.resolve(), 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// settings for view engine
+app.set('view engine', 'ejs');
+
+const isAuthenticated = async (req, res, next) => {
+    const { token } = req.cookies;
+    if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = await User.findById(decoded._id);
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+app.get('/', isAuthenticated, (req, res) => {
+    res.render('logout', { name: req.user.name });
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+// form action for Register
+app.post('/register', async (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+        return res.redirect('/login');
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10)
+ 
+
+    user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 60 * 1000)
+    });
+    res.redirect('/');
+});
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+// form action for login
+app.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (!user)  return res.redirect('/register');
+    
+
+    // const isMatch = user.password === password;
+    const isMatch = await bcrypt.compare( password, user.password);
+    if (!isMatch) return res.render('login', { email, message: "Incorrect Password" })
+    
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 60 * 1000)
+    });
+    res.redirect('/');
+});
+
+
+// form action for logout
+app.get('/logout', (req, res) => {
+    res.cookie('token', 'null', {
+        httpOnly: true,
+        expires: new Date(Date.now())
+    });
+    res.redirect('/');
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on http://localhost:${process.env.PORT}`);
+})
